4c7123a92c84c2251b8eb25f22389637cfc90e23
Pal_314 <4130.stkabirdin@gmail.com>
2025-09-08 12:55:16 +0530

    feat: Added bash script to start everything

---
+++ start.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+
+
+# Start PostgresQL if not running
+if ! brew services list | grep postgresql@15 | grep started > /dev/null; then
+  echo -e "${BLUE}Starting PostgreSQL...${NC}"
+  brew services start postgresql@15
+else
+  echo -e "${GREEN}PostgreSQL already running${NC}"
+fi
+
+# Start Redis if not running
+if ! brew services list | grep redis | grep started > /dev/null; then
+  echo -e "${BLUE}Starting Redis...${NC}"
+  brew services start redis
+else
+  echo -e "${GREEN}Redis already running${NC}"
+fi
+
+# Start Spring Boot
+./mvnw spring-boot:run
